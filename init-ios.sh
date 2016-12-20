04fd7d41bfa0e8fe58d609a053f693b25d52e4df
yuqilin <qilin.yu@mtime.com>
2016-12-20 10:55:47 +0800

    update repository

--- init-ios.sh
+++ init-ios.sh
@@ -17,12 +17,15 @@
 #
 
 # IJK_FFMPEG_UPSTREAM=git://git.videolan.org/ffmpeg.git
-IJK_FFMPEG_UPSTREAM=https://github.com/Bilibili/FFmpeg.git
-IJK_FFMPEG_FORK=https://github.com/Bilibili/FFmpeg.git
-IJK_FFMPEG_COMMIT=ff3.4--ijk0.8.7--20180103--001
+# IJK_FFMPEG_UPSTREAM=https://github.com/Bilibili/FFmpeg.git
+# IJK_FFMPEG_FORK=https://github.com/Bilibili/FFmpeg.git
+# IJK_FFMPEG_COMMIT=ff3.4--ijk0.8.7--20180103--001=======
+IJK_FFMPEG_UPSTREAM=https://github.com/yuqilin/FFmpeg.git
+IJK_FFMPEG_FORK=https://github.com/yuqilin/FFmpeg.git
+IJK_FFMPEG_COMMIT=qmediaplayer-dev
 IJK_FFMPEG_LOCAL_REPO=extra/ffmpeg
 
-IJK_GASP_UPSTREAM=https://github.com/Bilibili/gas-preprocessor.git
+IJK_GASP_UPSTREAM=https://github.com/yuqilin/gas-preprocessor.git
 
 # gas-preprocessor backup
 # https://github.com/Bilibili/gas-preprocessor.git
